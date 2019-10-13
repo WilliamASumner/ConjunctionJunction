3471ad98a65fac893ce3f8fd5be14c8bdbbf5df3
root <jbamberger23@gmail.com>
2019-10-13 19:57:41 -0400

    hey

---
+++ src/TrainController/TrainController.java
@@ -0,0 +1,11 @@
+public class TrainController(
+    string trainName;
+    trainModel trainModel;
+    
+    public TrainController(string name, trainModel tm){
+        trainName = name;
+        trainModel = tm;
+    }
+
+    
+)
