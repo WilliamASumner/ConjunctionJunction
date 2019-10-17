f37ecdcff88772767d9ce27c525996b74c562e8c
Will Sumner <williamasumner@gmail.com>
2019-10-16 22:20:43 -0400

    moved around ctc stuff

--- src/CTC/CTC.java
+++ src/CTC/CTC.java
@@ -1,36 +1,36 @@
 
 public class CTC{
-	TrainModel[] dispatchedTrains;
-	TrainModel newTrain;
-	TrainModel[] queuedTrains;
-	int throughput;
-	int currentTime;
-//	TrackControllerMain trckCntrl;
+    TrainModel[] dispatchedTrains;
+    TrainModel newTrain;
+    TrainModel[] queuedTrains;
+    int throughput;
+    int currentTime;
+//  TrackControllerMain trckCntrl;
 
-	public CTC(){
-//		trckCntrl = new TrackControllerMain();
-	}
-	
-	public void addTrain(String trainName, String auth, String spd){
-		// instantiate new train with given name, auth, and spd
-		double speed = Double.parseDouble(spd);
-		newTrain = new TrainModel(trainName, auth, speed);
-		// add train to currently dispatched trains array
-		//dispatchedTrains[0] = newTrain;
-//		trckCntrl.dispatchTrainData(speed, auth);
-	}
-	
-/*	
-	public void queueNewTrain(String trainName){
-		// Queue new train with given name
-	}
-	
-	public void dispatchQueuedTrain(String trainName){
-		// dispatch train
-	}
-	
-	public int sendAuthority(int trainsCurBlockID, int newAuthorityBlkID){
-		
-	}
-*/	
+    public CTC(){
+//      trckCntrl = new TrackControllerMain();
+    }
+    
+    public void addTrain(String trainName, String auth, String spd){
+        // instantiate new train with given name, auth, and spd
+        double speed = Double.parseDouble(spd);
+        newTrain = new TrainModel(trainName, auth, speed);
+        // add train to currently dispatched trains array
+        //dispatchedTrains[0] = newTrain;
+//      trckCntrl.dispatchTrainData(speed, auth);
+    }
+    
+/*  
+    public void queueNewTrain(String trainName){
+        // Queue new train with given name
+    }
+    
+    public void dispatchQueuedTrain(String trainName){
+        // dispatch train
+    }
+    
+    public int sendAuthority(int trainsCurBlockID, int newAuthorityBlkID){
+        
+    }
+*/  
 }
--- src/CTC/CTC_GUI.java
+++ src/CTC/CTC_GUI.java
@@ -31,208 +31,208 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 public class CTC_GUI extends Application {
-	// fields
-	private Label myLabel;
-	private TextField departureTime;
-	private TextField authority;
-	private TextField trainName;
-	private TextField speed;
-	private VBox rightVbox;
-	private ComboBox<String> comboAuthority;
-	private String cssLayout = "-fx-border-color: black;\n" +
+    // fields
+    private Label myLabel;
+    private TextField departureTime;
+    private TextField authority;
+    private TextField trainName;
+    private TextField speed;
+    private VBox rightVbox;
+    private ComboBox<String> comboAuthority;
+    private String cssLayout = "-fx-border-color: black;\n" +
                    "-fx-border-insets: 0;\n" +
                    "-fx-border-width: 1;\n";
-	static CTC newCTC;
-	
+    static CTC newCTC;
+    
     public static void main(String[] args) {
-		// launch CTC 
-		newCTC = new CTC();
-		// launch the app
+        // launch CTC 
+        newCTC = new CTC();
+        // launch the app
         launch();
     }
-	
-	@Override
+    
+    @Override
     public void start(Stage stage) {
-		
-		// Create a BorderPane object
-		BorderPane borderPane = new BorderPane();
-	//	borderPane.setStyle(cssLayout);
-		
-		//--------------------------------------------------------------
-		// Add image of track
-		Image trackLayout = new Image("file:trackLayout.jpg");
-		// create an ImageViw object
-		ImageView imageView = new ImageView(trackLayout);
-		imageView.setFitWidth(500);
-		imageView.setFitHeight(350);
-		// put imageView into center of BorderPane
-		borderPane.setCenter(imageView);
-		//---------------------------------------------------------------
-		
-		
-		//---------------------------------------------------------------
-		// The bottom portion of the BorderPane Layout Container
-		// Create 'maintain track' view
-		HBox hboxMaintain = new HBox(10, new Label("Choose Track"), new Label("Choose Block"));
-		// Create a VBox
-		VBox vboxMaintain = new VBox(10, new Label("Track Maintenance"), hboxMaintain, new Label("Maintenance Type"));
-		vboxMaintain.setStyle(cssLayout);
+        
+        // Create a BorderPane object
+        BorderPane borderPane = new BorderPane();
+    //  borderPane.setStyle(cssLayout);
+        
+        //--------------------------------------------------------------
+        // Add image of track
+        Image trackLayout = new Image("file:trackLayout.jpg");
+        // create an ImageViw object
+        ImageView imageView = new ImageView(trackLayout);
+        imageView.setFitWidth(500);
+        imageView.setFitHeight(350);
+        // put imageView into center of BorderPane
+        borderPane.setCenter(imageView);
+        //---------------------------------------------------------------
+        
+        
+        //---------------------------------------------------------------
+        // The bottom portion of the BorderPane Layout Container
+        // Create 'maintain track' view
+        HBox hboxMaintain = new HBox(10, new Label("Choose Track"), new Label("Choose Block"));
+        // Create a VBox
+        VBox vboxMaintain = new VBox(10, new Label("Track Maintenance"), hboxMaintain, new Label("Maintenance Type"));
+        vboxMaintain.setStyle(cssLayout);
 
-		// Create 'Add Train' view
-//		ListView<String> addTrains = new ListView<>();
-/**		
-		String[] StationNames = {"G1","G2","G3"};
+        // Create 'Add Train' view
+//      ListView<String> addTrains = new ListView<>();
+/**     
+        String[] StationNames = {"G1","G2","G3"};
         ComboBox controllerBox = new ComboBox();
         for (String option: ControllerNames) {
             controllerBox.getItems().add(option);
         }
-        controllerBox.getSelectionModel().selectFirst();		
-*/		
-		String[] StationNames = {"PIONEER","EDGEBROOK","STATION", "WHITED", 
-									"SOUTH BANK", "CENTRAL", "INGLEWOOD",
-									"OVERBROOK", "GLENBURY", "DORMONT",
-									"MT LEBANON", "POPLAR", "CASTLE SHANNON"};
+        controllerBox.getSelectionModel().selectFirst();        
+*/      
+        String[] StationNames = {"PIONEER","EDGEBROOK","STATION", "WHITED", 
+                                    "SOUTH BANK", "CENTRAL", "INGLEWOOD",
+                                    "OVERBROOK", "GLENBURY", "DORMONT",
+                                    "MT LEBANON", "POPLAR", "CASTLE SHANNON"};
         comboAuthority = new ComboBox<>();
         for (String option: StationNames)
             comboAuthority.getItems().add(option);
         comboAuthority.getSelectionModel().selectFirst();
-		
-		
-		Label promptLabelTName = new Label("Train name: ");
-		// create a TextField for input
-		trainName = new TextField();		
-		
-		Label promptLabelAuth = new Label("Authority (Station Name): ");
-		// create a TextField for input
-	//	authority = new TextField();		
-		
-		Label promptLabelSpeed = new Label("Speed in MPH: ");
-		// create a TextField for input
-		speed = new TextField();		
-		
-		// create a label to display a prompt
-		Label promptLabelDepart = new Label("Dispatch time in form: 00:00:00");
-		// create a TextField for input
-		departureTime = new TextField();
-		
-		// Create button
-		Button dispatchT = new Button("Dispatch Train");
-		
-		// Register the event handler
-		dispatchT.setOnAction(new DispatchButtonHandler());
-		
-		// create an empty label to display dispatched train
-		myLabel = new Label();
-		
-		// Put in HBoxAdd
-		HBox hboxAdd1 = new HBox(10, promptLabelTName, trainName, promptLabelAuth, comboAuthority);
-		HBox hboxAdd2 = new HBox(10, promptLabelSpeed, speed, promptLabelDepart, departureTime);
-		
-		// Create StackPane for pronounced viewer section title
-		StackPane stackAdd = new StackPane();
-		Rectangle addTrainRec = new Rectangle(600.0, 25.0);
-		addTrainRec.setFill(new LinearGradient(0,0,0,1, true, CycleMethod.NO_CYCLE,
-		new Stop[]{
+        
+        
+        Label promptLabelTName = new Label("Train name: ");
+        // create a TextField for input
+        trainName = new TextField();        
+        
+        Label promptLabelAuth = new Label("Authority (Station Name): ");
+        // create a TextField for input
+    //  authority = new TextField();        
+        
+        Label promptLabelSpeed = new Label("Speed in MPH: ");
+        // create a TextField for input
+        speed = new TextField();        
+        
+        // create a label to display a prompt
+        Label promptLabelDepart = new Label("Dispatch time in form: 00:00:00");
+        // create a TextField for input
+        departureTime = new TextField();
+        
+        // Create button
+        Button dispatchT = new Button("Dispatch Train");
+        
+        // Register the event handler
+        dispatchT.setOnAction(new DispatchButtonHandler());
+        
+        // create an empty label to display dispatched train
+        myLabel = new Label();
+        
+        // Put in HBoxAdd
+        HBox hboxAdd1 = new HBox(10, promptLabelTName, trainName, promptLabelAuth, comboAuthority);
+        HBox hboxAdd2 = new HBox(10, promptLabelSpeed, speed, promptLabelDepart, departureTime);
+        
+        // Create StackPane for pronounced viewer section title
+        StackPane stackAdd = new StackPane();
+        Rectangle addTrainRec = new Rectangle(600.0, 25.0);
+        addTrainRec.setFill(new LinearGradient(0,0,0,1, true, CycleMethod.NO_CYCLE,
+        new Stop[]{
         new Stop(0,Color.web("#C0C0C0")),
         new Stop(0.5, Color.web("#B1B1B1")),
         new Stop(1,Color.web("#A2A2A2")),}));
-		addTrainRec.setStroke(Color.web("#CFCFCF"));
-		addTrainRec.setArcHeight(3.5);
-		addTrainRec.setArcWidth(3.5);
-		Text textAdd = new Text("Add Train");
-		stackAdd.getChildren().addAll(addTrainRec, textAdd);
-		stackAdd.setAlignment(Pos.CENTER_LEFT);          
-		
-		VBox vboxAdd = new VBox(10, stackAdd, hboxAdd1, hboxAdd2, dispatchT);
-		vboxAdd.setStyle(cssLayout);
+        addTrainRec.setStroke(Color.web("#CFCFCF"));
+        addTrainRec.setArcHeight(3.5);
+        addTrainRec.setArcWidth(3.5);
+        Text textAdd = new Text("Add Train");
+        stackAdd.getChildren().addAll(addTrainRec, textAdd);
+        stackAdd.setAlignment(Pos.CENTER_LEFT);          
+        
+        VBox vboxAdd = new VBox(10, stackAdd, hboxAdd1, hboxAdd2, dispatchT);
+        vboxAdd.setStyle(cssLayout);
 
-		// Create 'Select a Queued Train' view
-//		HBox hboxQueued = new HBox(10, new Label("Choose Track"), new Label("Choose Block"));
-		// Create a VBox
-		VBox vboxQueued = new VBox(10, new Label("Select a Queued Train"), new Label("Queued Trains"));
-		vboxQueued.setStyle(cssLayout);
-	
-		// Create an HBox to contain the different functions for the dispatcher
-		HBox bottomControlOfModule = new HBox(10, vboxMaintain, vboxAdd, vboxQueued);
-		
-		
-		// Add to bottom of BorderPane
-		borderPane.setBottom(bottomControlOfModule);
-		//---------------------------------------------------------------
-		
-		
-		//---------------------------------------------------------------
-		// Create right BorderPane
-		Label name = new Label("Train Name");
-		Label block = new Label("Current Block");
-		Label a = new Label("Authority");
-		Label spd = new Label("Speed");
-		
-		HBox dispatchedTrainsViewer = new HBox(10, name, block, a, spd);
-		
-		// Create StackPane for pronounced viewer section title
-		StackPane stackDepat = new StackPane();
-		Rectangle departTrainRec = new Rectangle(250.0, 25.0);
-		departTrainRec.setFill(new LinearGradient(0,0,0,1, true, CycleMethod.NO_CYCLE,
-		new Stop[]{
+        // Create 'Select a Queued Train' view
+//      HBox hboxQueued = new HBox(10, new Label("Choose Track"), new Label("Choose Block"));
+        // Create a VBox
+        VBox vboxQueued = new VBox(10, new Label("Select a Queued Train"), new Label("Queued Trains"));
+        vboxQueued.setStyle(cssLayout);
+    
+        // Create an HBox to contain the different functions for the dispatcher
+        HBox bottomControlOfModule = new HBox(10, vboxMaintain, vboxAdd, vboxQueued);
+        
+        
+        // Add to bottom of BorderPane
+        borderPane.setBottom(bottomControlOfModule);
+        //---------------------------------------------------------------
+        
+        
+        //---------------------------------------------------------------
+        // Create right BorderPane
+        Label name = new Label("Train Name");
+        Label block = new Label("Current Block");
+        Label a = new Label("Authority");
+        Label spd = new Label("Speed");
+        
+        HBox dispatchedTrainsViewer = new HBox(10, name, block, a, spd);
+        
+        // Create StackPane for pronounced viewer section title
+        StackPane stackDepat = new StackPane();
+        Rectangle departTrainRec = new Rectangle(250.0, 25.0);
+        departTrainRec.setFill(new LinearGradient(0,0,0,1, true, CycleMethod.NO_CYCLE,
+        new Stop[]{
         new Stop(0,Color.web("#C0C0C0")),
         new Stop(0.5, Color.web("#B1B1B1")),
         new Stop(1,Color.web("#A2A2A2")),}));
-		departTrainRec.setStroke(Color.web("#CFCFCF"));
-		departTrainRec.setArcHeight(3.5);
-		departTrainRec.setArcWidth(3.5);
-		Text textDepart = new Text("Dispatched Trains");
-		stackDepat.getChildren().addAll(departTrainRec, textDepart);
-		stackDepat.setAlignment(Pos.CENTER_LEFT);
-		
-		// Add to a VBox
-		rightVbox = new VBox(10, stackDepat, dispatchedTrainsViewer);		
-		rightVbox.setStyle(cssLayout);
-		
-		borderPane.setRight(rightVbox);
-		//---------------------------------------------------------------
-		
-		
-		//---------------------------------------------------------------
-		// Create left BorderPane
-		// Add image of clock
-		Image clock = new Image("file:digital_clock.jpg");
-		// create an ImageViw object
-		ImageView imageView2 = new ImageView(clock);
-		imageView2.setFitWidth(250);
-		imageView2.setFitHeight(150);
-		
-		VBox leftVbox = new VBox(10, imageView2, new Label("Throughput = 0"));
-		leftVbox.setStyle(cssLayout);
-		// put imageView2 into left of BorderPane
-		borderPane.setLeft(leftVbox);
-		//----------------------------------------------------------------
-		
-		
-		// Create a scene with the borderPane as its root node
+        departTrainRec.setStroke(Color.web("#CFCFCF"));
+        departTrainRec.setArcHeight(3.5);
+        departTrainRec.setArcWidth(3.5);
+        Text textDepart = new Text("Dispatched Trains");
+        stackDepat.getChildren().addAll(departTrainRec, textDepart);
+        stackDepat.setAlignment(Pos.CENTER_LEFT);
+        
+        // Add to a VBox
+        rightVbox = new VBox(10, stackDepat, dispatchedTrainsViewer);       
+        rightVbox.setStyle(cssLayout);
+        
+        borderPane.setRight(rightVbox);
+        //---------------------------------------------------------------
+        
+        
+        //---------------------------------------------------------------
+        // Create left BorderPane
+        // Add image of clock
+        Image clock = new Image("file:digital_clock.jpg");
+        // create an ImageViw object
+        ImageView imageView2 = new ImageView(clock);
+        imageView2.setFitWidth(250);
+        imageView2.setFitHeight(150);
+        
+        VBox leftVbox = new VBox(10, imageView2, new Label("Throughput = 0"));
+        leftVbox.setStyle(cssLayout);
+        // put imageView2 into left of BorderPane
+        borderPane.setLeft(leftVbox);
+        //----------------------------------------------------------------
+        
+        
+        // Create a scene with the borderPane as its root node
         Scene scene = new Scene(borderPane);//(new StackPane(l), 640, 480);
-		stage.setTitle("CTC Module");
+        stage.setTitle("CTC Module");
         stage.setScene(scene);
         stage.show();
     }
 
-	/**
-	 * Event handler class for dispatchT button.
-	 */
-	class DispatchButtonHandler implements EventHandler<ActionEvent>{
-		@Override
-		public void handle(ActionEvent event){
-			String departTime = departureTime.getText();
-			String auth = comboAuthority.getValue();
-			String tName = trainName.getText();
-			String speedMPH = speed.getText();
-			myLabel.setText(tName + "\t\tK63" + "\t\t" + auth + "\t\t" + speedMPH);
-			rightVbox.getChildren().add(myLabel);
-			
-			//CREATE NEW TRAIN IN CTC
-			newCTC.addTrain(tName, auth, speedMPH);
-			// SEND DISPATCHED TRAIN INFO TO TRACK CONTROLLER
-			//CTC.dispatchQueuedTrain(tName, auth, sp);
-		}
-	}
+    /**
+     * Event handler class for dispatchT button.
+     */
+    class DispatchButtonHandler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent event){
+            String departTime = departureTime.getText();
+            String auth = comboAuthority.getValue();
+            String tName = trainName.getText();
+            String speedMPH = speed.getText();
+            myLabel.setText(tName + "\t\tK63" + "\t\t" + auth + "\t\t" + speedMPH);
+            rightVbox.getChildren().add(myLabel);
+            
+            //CREATE NEW TRAIN IN CTC
+            newCTC.addTrain(tName, auth, speedMPH);
+            // SEND DISPATCHED TRAIN INFO TO TRACK CONTROLLER
+            //CTC.dispatchQueuedTrain(tName, auth, sp);
+        }
+    }
 }
