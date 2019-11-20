ed0c2c18366abe62fe623b01048e30fc5638899f
Natasha Kamtekar <nkamtekar@thirdeyegen.com>
2019-11-20 17:13:35 -0500

    fixed TkM and TrackMap and TkMGUI

--- src/Block.java
+++ src/Block.java
@@ -139,4 +139,8 @@ public class Block
         return type;
     }
 
+    public String toString() {
+      return this.getBlockID();
+    }
+
 }
--- src/TrackMap.java
+++ src/TrackMap.java
@@ -12,10 +12,10 @@ public class TrackMap {
 
   }
 
-  public void parseFile(String filename) {
-    map.add(0, null);
+  public ArrayList<Block> parseFile(String filename) {
+    map.add(0, new Block());
     if (!(filename.substring(filename.length()-3,filename.length()).equals("csv"))) {
-      return;
+      return map;
     }
 
     String csv = "";
@@ -28,11 +28,11 @@ public class TrackMap {
       //System.out.println(csv);
     }
     catch (FileNotFoundException e){
-      return;
+      return map;
     }
 
     String[] csvrows = csv.split("\n");
-    Block newBlock = new Block();
+
 
     // 11 rows
 
@@ -46,8 +46,9 @@ public class TrackMap {
       // for (int j = 0; j <= 8; j++) {
       //   System.out.print(elements[j] + " ");
       // }
+        Block newBlock = new Block();
         newBlock.setLineColor(elements[0]);
-        newBlock.setBlockID(elements[2]+elements[1]+"");
+        newBlock.setBlockID(elements[1]+elements[2]+"");
         newBlock.setLength(Double.parseDouble(elements[3]));
         newBlock.setGrade(Double.parseDouble(elements[4]));
         newBlock.setSpeedLimit(Double.parseDouble(elements[5]));
@@ -56,15 +57,19 @@ public class TrackMap {
         }
         newBlock.setElevation(Double.parseDouble(elements[8]));
         int index = Integer.parseInt(elements[2]);
-        //System.out.println(index);
+        //System.out.println(index + ", " + newBlock.getBlockID());
         map.add(index, newBlock);
-      //  System.out.println(newBlock.getBlockID() + ", " + newBlock.getLineColor());
-
-
-
+        // System.out.println(newBlock.getBlockID() + ", " + newBlock.getLineColor());
+        // System.out.println("THIS" + map.get(index).getBlockID());
+        // System.out.println("LAST"+map.get(index-1).getBlockID());
 
     }
 
+    // System.out.println(map.get(1).getBlockID());
+    // System.out.println(map.get(2).getBlockID());
+    // System.out.println(map.get(3).getBlockID());
+
+    return map;
 
   }
 
@@ -80,6 +85,18 @@ public class TrackMap {
     return map.get(blockID);
   }
 
+  public ArrayList<Block> getBlocksBySection(String section) {
+    ArrayList<Block> bySection = new ArrayList<Block>();
+    for (int i = 1; i < map.size(); i++) {
+      Block b = map.get(i);
+      String s = b.getBlockID().charAt(0)+"";
+      if (s.equals(section)){
+        bySection.add(map.get(i));
+      }
+    }
+    return bySection;
+  }
+
 
 
   public void updateBlock(Block block) {
@@ -89,11 +106,13 @@ public class TrackMap {
   }
 
 
-  // public static void main(String[] args) {
-  //   TrackMap t = new TrackMap();
-  //
-  //   t.parseFile("redFile.csv");
-  // }
+  public static void main(String[] args) {
+    TrackMap t = new TrackMap();
+
+    t.parseFile("redFile.csv");
+    ArrayList<Block> aaa = t.getBlocksBySection("A");
+    System.out.println(aaa.size());
+  }
 
 
 }
