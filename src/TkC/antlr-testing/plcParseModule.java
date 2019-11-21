93773670a6fe042907eebdae0bbbe17da1db8de0
Will Sumner <williamasumner@gmail.com>
2019-11-21 14:57:40 -0500

    temporarily removed parseModule code

--- src/TkC/antlr-testing/plcParseModule.java
+++ src/TkC/antlr-testing/plcParseModule.java
@@ -7,7 +7,7 @@ public class plcParseModule
     public static void main (String args[]) throws IOException
     {
         // Create a scanner to read from keyboard
-        ANTLRFileStream inFile = new ANTLRFileStream("test.plc");
+        /*ANTLRFileStream inFile = new ANTLRFileStream("test.plc");
         TkcLexer lex = new TkcLexer(null);
         lex.setInputStream(inFile);
 
@@ -18,7 +18,7 @@ public class plcParseModule
         parser.setBuildParseTree(true);
         parser.setTokenStream(tokens);
 
-        ParserRulContext tree = parser.program();
+        ParserRulContext tree = parser.program();*/
 
     }
 }
