855cd5e6a25eefd9e43325219905d7c4d161c26e
jbamberger23 <35778852+jbamberger23@users.noreply.github.com>
2019-11-21 18:04:36 -0500

    Update TrainControllerGUI.java

--- src/TrainController/TrainControllerGUI.java
+++ src/TrainController/TrainControllerGUI.java
@@ -48,15 +48,18 @@ import javafx.scene.image.*;
 public class TrainControllerGUI extends Application implements EventHandler<ActionEvent> {
     double speed;
     String authority;
+    double powerCommand;
     String name;
     TrainController tnc;
 
     Slider speedSlider;
 
+    FlowPane flowpane;
+
     //Buttons on TrainControllerGUI
     Button setSpeedButton = new Button("Set Speed");
     Button eBrake = new Button("E BRAKE: Currently OFF");
-    Button sBrake = new Button("Service Brake");
+    Button sBrake = new Button("S BRAKE: Currently OFF");
     Button lights = new Button("Turn Lights ON");
 
 
@@ -69,6 +72,10 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
     Button lDoor2 = new Button("Left Door 2");
     Button lDoor3 = new Button("Left Door 3");
     Button lDoor4 = new Button("Left Door 4");
+
+    Label driverSetSpeed;
+    Label currentTemp;
+    Label powerLabel;
 
     RadioButton autoRadioButton;
     RadioButton manualRadioButton;
@@ -87,7 +94,7 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
 	  //authority = TrainController.authority;
 	  //name = TrainController.trainName;
     }
-    public TrainControllerGUI(Stage primaryStage, TrainController tc) {
+    public TrainControllerGUI(TrainController tc) {
       /*
       speed = TrainController.getAuditedSpeed();
       authority = TrainController.getAuthority();
@@ -96,29 +103,30 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
       tnc = tc;
 	  
     }
-    
 
-    @Override
-       public void start(Stage primaryStage) {
-        //Update the GUI information with the specific train controller object's data
-        //speed = tnc.getAuditedSpeed();
-        //authority = tnc.getAuthority();
-        //name = tnc.getName();
+    public void updatePowerCommand(){
+        powerCommand = tnc.calculatePower();
+       // powerLabel.setText("Current Power: " + Double.toString(powerCommand) + "kWatts");
+    }
+
+    public void initGUI(){
         autoRadioButton = new RadioButton("Automatic");
         setTempButton = new Button("Set Temp");
-        
-
-        //primaryStage.setTitle(name + " - Train Controller UI");
 
         eBrake.setStyle("-fx-text-fill: red");
         eBrake.setMinWidth(400);
         eBrake.setMaxWidth(400);
-        eBrake.setMinWidth(100);
-        eBrake.setMaxWidth(100);
-           
+        eBrake.setMinWidth(200);
+        eBrake.setMaxWidth(200);
 
-        //Label output = new Label("Train Name: " + name + "\nAudited Authority: " + authority + "\nAudited Speed Limit: " + speed); 
-          
+        sBrake.setStyle("-fx-text-fill: blue");
+        sBrake.setMinWidth(400);
+        sBrake.setMaxWidth(400);
+        sBrake.setMinWidth(200);
+        sBrake.setMaxWidth(200);
+
+        powerLabel = new Label("Current Power: 0 kWatts");
+
         Label modeSelectLabel = new Label("Select a Mode: "); 
         Label doorLabel = new Label("Door Status Control: "); 
 
@@ -130,89 +138,118 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
    
         ToggleGroup radioGroup = new ToggleGroup();
 
-        //speedSlider = new Slider(0, speed, 0);
-        speedSlider = new Slider(0, 80, 0);
-        speedSlider.setShowTickLabels(true);
+         speedSlider = new Slider(0, tnc.getSetSpeed(), 0);
+         //speedSlider = new Slider(0, 80, 0);
+         speedSlider.setShowTickLabels(true);
 
-        tempSlider = new Slider(0, 80, 0);
-        tempSlider.setShowTickLabels(true);
-   
-        autoRadioButton.setToggleGroup(radioGroup);
-        manualRadioButton.setToggleGroup(radioGroup);
 
-        HBox hbox = new HBox(autoRadioButton, manualRadioButton);
+        //Uncomment first one when integrated with train model
+         //driverSetSpeed = new Label("Current Set Speed Limit" + tnc.getSetSpeed());
+         driverSetSpeed = new Label("Current Set Speed Limit " + tnc.getSetSpeed() + " MPH");
 
-           
-        FlowPane flowpane = new FlowPane();
 
+
+         currentTemp = new Label("Current Temperature: " + tnc.getTemp() + " Fahrenheit");
+
+         tempSlider = new Slider(0, 80, 0);
+         tempSlider.setShowTickLabels(true);
+    
+         autoRadioButton.setToggleGroup(radioGroup);
+         manualRadioButton.setToggleGroup(radioGroup);
+ 
+         HBox hbox = new HBox(autoRadioButton, manualRadioButton);
+ 
+            
+         flowpane = new FlowPane();
+ 
+ 
+         FlowPane flowpane1 = new FlowPane();
+         FlowPane speed = new FlowPane();
+         FlowPane temp = new FlowPane();
+         flowpane1.setStyle("-fx-border-color: black");
+         speed.getChildren().add(speedSlider);
+         speed.getChildren().add(setSpeedButton);
+         speed.getChildren().add(driverSetSpeed);
+         
+         temp.getChildren().add(tempSlider);  
+         temp.getChildren().add(setTempButton);
+         temp.getChildren().add(currentTemp);
+
+         flowpane1.getChildren().add(powerLabel);
+         flowpane1.getChildren().add(speed);
+         flowpane1.getChildren().add(temp);
+         flowpane.getChildren().add(flowpane1);
+        // flowpane1.getChildren().add(output);
         
  
+ 
+         //Panel for mode select
+         FlowPane modeSelect = new FlowPane();
+         modeSelect.setStyle("-fx-border-color: black");
+         flowpane.getChildren().add(modeSelect);
+         modeSelect.getChildren().add(modeSelectLabel);
+         modeSelect.getChildren().add(hbox);
+ 
+         //Panel for failure status
+         FlowPane failure = new FlowPane();
+         failure.setStyle("-fx-border-color: black");
+         flowpane.getChildren().add(failure);
+         failure.getChildren().add(failureTitle);
+         failure.getChildren().add(failureList);
+ 
+         FlowPane brake = new FlowPane();
+         brake.setStyle("-fx-border-color: black");
+         //emergency brake
+         brake.getChildren().add(eBrake);
+         //service brake
+         brake.getChildren().add(sBrake);
+         brake.getChildren().add(lights);
+         flowpane.getChildren().add(brake);
+           
+ 
+         //Panel for Door Status
+         FlowPane doorStatus = new FlowPane();
+         doorStatus.setStyle("-fx-border-color: black");
+         doorStatus.getChildren().add(doorLabel);
+         doorStatus.getChildren().add(rDoor1);
+         doorStatus.getChildren().add(rDoor2);
+         doorStatus.getChildren().add(rDoor3);
+         doorStatus.getChildren().add(rDoor4);
+         doorStatus.getChildren().add(lDoor1);
+         doorStatus.getChildren().add(lDoor2);
+         doorStatus.getChildren().add(lDoor3);
+         doorStatus.getChildren().add(lDoor4);
+         flowpane.getChildren().add(doorStatus);
+ 
+         setSpeedButton.setOnAction(new setSpeedHandler());
+         eBrake.setOnAction(new eBrakeHandler());
+         sBrake.setOnAction(new sBrakeHandler());
+         lights.setOnAction(new lightsHandler());
+         rDoor1.setOnAction(new rDoor1Handler());
+         rDoor2.setOnAction(new rDoor2Handler());
+         rDoor3.setOnAction(new rDoor3Handler());
+         rDoor4.setOnAction(new rDoor4Handler());
+         lDoor1.setOnAction(new lDoor1Handler());
+         lDoor2.setOnAction(new lDoor2Handler());
+         lDoor3.setOnAction(new lDoor3Handler());
+         lDoor4.setOnAction(new lDoor4Handler());
+         setTempButton.setOnAction(new tempHandler());
+         autoRadioButton.setOnAction(new autoModeHandler());
+         manualRadioButton.setOnAction(new manualModeHandler());
 
-        //Panel for output for iteration 2
-        FlowPane flowpane1 = new FlowPane();
-        flowpane1.setStyle("-fx-border-color: black");
-        flowpane.getChildren().add(flowpane1);
-       // flowpane1.getChildren().add(output);
-        flowpane1.getChildren().add(speedSlider);
-        flowpane1.getChildren().add(setSpeedButton);
-        flowpane1.getChildren().add(tempSlider);  
-        flowpane1.getChildren().add(setTempButton);
-
-
-        //Panel for mode select
-        FlowPane modeSelect = new FlowPane();
-        modeSelect.setStyle("-fx-border-color: black");
-        flowpane.getChildren().add(modeSelect);
-        modeSelect.getChildren().add(modeSelectLabel);
-        modeSelect.getChildren().add(hbox);
-
-        //Panel for failure status
-        FlowPane failure = new FlowPane();
-        failure.setStyle("-fx-border-color: black");
-        flowpane.getChildren().add(failure);
-        failure.getChildren().add(failureTitle);
-        failure.getChildren().add(failureList);
-
-        FlowPane brake = new FlowPane();
-        brake.setStyle("-fx-border-color: black");
-        //emergency brake
-        brake.getChildren().add(eBrake);
-        //service brake
-        brake.getChildren().add(sBrake);
-        brake.getChildren().add(lights);
-        flowpane.getChildren().add(brake);
-          
-
-        //Panel for Door Status
-        FlowPane doorStatus = new FlowPane();
-        doorStatus.setStyle("-fx-border-color: black");
-        doorStatus.getChildren().add(doorLabel);
-        doorStatus.getChildren().add(rDoor1);
-        doorStatus.getChildren().add(rDoor2);
-        doorStatus.getChildren().add(rDoor3);
-        doorStatus.getChildren().add(rDoor4);
-        doorStatus.getChildren().add(lDoor1);
-        doorStatus.getChildren().add(lDoor2);
-        doorStatus.getChildren().add(lDoor3);
-        doorStatus.getChildren().add(lDoor4);
-        flowpane.getChildren().add(doorStatus);
         
-        setSpeedButton.setOnAction(new setSpeedHandler());
-        eBrake.setOnAction(new eBrakeHandler());
-        sBrake.setOnAction(new sBrakeHandler());
-        lights.setOnAction(new lightsHandler());
-        rDoor1.setOnAction(new rDoor1Handler());
-        rDoor2.setOnAction(new rDoor2Handler());
-        rDoor3.setOnAction(new rDoor3Handler());
-        rDoor4.setOnAction(new rDoor4Handler());
-        lDoor1.setOnAction(new lDoor1Handler());
-        lDoor2.setOnAction(new lDoor2Handler());
-        lDoor3.setOnAction(new lDoor3Handler());
-        lDoor4.setOnAction(new lDoor4Handler());
-        setTempButton.setOnAction(new tempHandler());
-        autoRadioButton.setOnAction(new autoModeHandler());
-        manualRadioButton.setOnAction(new manualModeHandler());
+    }
+    
 
+    @Override
+       public void start(Stage primaryStage) {
+        initGUI();
+        
+        //primaryStage.setTitle(name + " - Train Controller UI");
+
+        
+        //Label output = new Label("Train Name: " + name + "\nAudited Authority: " + authority + "\nAudited Speed Limit: " + speed); 
+        
         Scene scene = new Scene(flowpane, 500, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -222,7 +259,10 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
       public class setSpeedHandler implements EventHandler<ActionEvent>{
         @Override
         public void handle(ActionEvent event){
-            tnc.newDriverSetSpeed(speedSlider.getValue());
+            boolean test = tnc.setNewSpeed(speedSlider.getValue());
+            if(test){
+                driverSetSpeed.setText("Current Set Speed Limit: " + speedSlider.getValue() + " MPH");
+            }
         }
       }
 
@@ -248,10 +288,10 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
             boolean currentState;
                 currentState = tnc.toggleServiceBrake();
                 if(currentState){
-                    eBrake.setText("S Brake: Currently ON");
+                    sBrake.setText("S Brake: Currently ON");
                 }
                 else{
-                    eBrake.setText("S Brake: Currently OFF");
+                    sBrake.setText("S Brake: Currently OFF");
 
                 }
         }
@@ -386,6 +426,7 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
           @Override
           public void handle(ActionEvent event){
             tnc.setTemp(tempSlider.getValue());
+            currentTemp.setText("Current Temperature: " + tnc.getTemp() + " Fahrenheit");
           }
       }
       
@@ -416,6 +457,5 @@ public class TrainControllerGUI extends Application implements EventHandler<Acti
 
 
 }
-
 
 
