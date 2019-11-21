74e2aec7a1c791c93185685a3ef3d7c3faa53c0d
Will Sumner <williamasumner@gmail.com>
2019-11-21 17:07:24 -0500

    fixed grammar

--- src/TkC/antlr-testing/Interpreter.java
+++ src/TkC/antlr-testing/Interpreter.java
@@ -1,15 +1,12 @@
-public class MyListener //extends TkcBaseListener
+public class Interpreter extends TkcBaseListener
 {
     ActionsList actionList;
     TrackController tkc;
 
-    public MyListener(TrackController trkcc) {
+    public Interpreter(TrackController trkcc) {
         tkc = trkcc;
         actionList = new ActionsList(tkc);
     }
-
-
-    /*
 
     @Override
     public void enterProgram(TkcParser.ProgramContext ctx) {
@@ -82,5 +79,5 @@ public class MyListener //extends TkcBaseListener
     @Override
     public void exitValue(TkcParser.ValueContext ctx) {
 
-    } */
+    }
 }
