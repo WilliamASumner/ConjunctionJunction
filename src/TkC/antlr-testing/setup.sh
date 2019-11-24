eb98b6c301ec96c80190df6c5ce693f93ce50227
Will Sumner <williamasumner@gmail.com>
2019-11-24 00:28:57 +0000

    fixed setup.sh

--- src/TkC/antlr-testing/setup.sh
+++ src/TkC/antlr-testing/setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 if [[ "$SHLVL" != "1" ]]; then
-    echo "Run this script with source please."
-    exit
+   echo "Run this script with source please."
+   exit
 fi
+
 OS=`uname`
 if [[ "$OS" == "Darwin" ]]; then
     LIBPATH="/Users/willsumner/Desktop/School/Software-Engineering/ConjunctionJunction/lib"
 else
     LIBPATH="/home/jbamb/COE1186/iter3/ConjunctionJuntion/lib"
 fi
+LIBPATH=`pwd | grep -o ".*ConjunctionJunction"`
+LIBPATH="$LIBPATH/lib"
+
 if [ -z $ANTLRPATH ]; then
     export CLASSPATH=".:$LIBPATH/antlr-4.7.2-complete.jar:$CLASSPATH"
 fi
+
 export ANTLRPATH=`realpath --relative-to $(pwd) $LIBPATH/antlr-4.7.2-complete.jar`
 if [[ "$OS" == "Darwin" ]]; then
     alias grun='java -Xmx500M -cp ".:$ANTLRPATH" org.antlr.v4.gui.TestRig'
