776b083b6c3366d3369d7213490a1abfc7f94f46
Will Sumner <williamasumner@gmail.com>
2019-11-25 20:56:39 +0000

    added EvaluationList generation to antlr listener

--- src/TkC/parsing/EvaluatorListener.java
+++ src/TkC/parsing/EvaluatorListener.java
@@ -1,120 +1,160 @@
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.misc.Interval;
 
 public class EvaluatorListener extends TkcBaseListener {
 
     TkcParser parser;
+    EvalList list;
+    TrackController tkc;
 
-    public EvaluatorListener(TkcParser p) {
+    IfNode currentIf;
+    Condition currentCondition;
+    Action currentAction;
+    ChainType chainOp = ChainType.NONE;
+    boolean parsingError = false;
+
+    public EvaluatorListener(TkcParser p, TrackController t) {
 
         p=parser;
+        list = new EvalList();
+        tkc = t;
+
+        currentIf = null;
+        currentCondition = null;
+        currentAction = null;
+
+        chainOp = ChainType.NONE;
+        parsingError = false;
     }
 
-
-    @Override
-    public void enterProgram(TkcParser.ProgramContext ctx) {
-        System.out.println("Entering Program");
+    public boolean encounteredError() {
+        return parsingError;
     }
 
-    @Override
-    public void exitProgram(TkcParser.ProgramContext ctx) {
-        System.out.println("Exiting Program");
-    }
-
-    /*@Override
-    public void enterIfsequence(TkcParser.IfsequenceContext ctx) {
-    }
-
-    @Override
-    public void exitIfsequence(TkcParser.IfsequenceContext ctx) {
+    public EvalList getEvalList() {
+        if (!parsingError) {
+            return list;
+        }
+        return null;
     }
 
     @Override
     public void enterIfstatement(TkcParser.IfstatementContext ctx) {
+        currentIf = new IfNode();
+
     }
 
     @Override
     public void exitIfstatement(TkcParser.IfstatementContext ctx) {
+        if (currentIf != null) {
+            list.addIf(currentIf);
+        }
+        currentIf = null;
     }
 
     @Override
-    public void enterConditionlist(TkcParser.ConditionlistContext ctx) {
+    public void enterSingleCondition(TkcParser.SingleConditionContext ctx) {
+        currentCondition = new Condition();
+        currentCondition.setChainOp(ChainType.OR);
     }
 
     @Override
-    public void exitConditionlist(TkcParser.ConditionlistContext ctx) {
-    }*/
+    public void enterConditionListOr(TkcParser.ConditionListOrContext ctx) {
+        currentCondition = new Condition();
+        currentCondition.setChainOp(ChainType.OR);
+    }
+
+    @Override
+    public void enterConditionListAnd(TkcParser.ConditionListAndContext ctx) {
+        currentCondition = new Condition();
+        currentCondition.setChainOp(ChainType.AND);
+    }
 
     @Override
     public void enterCondition(TkcParser.ConditionContext ctx) {
-        System.out.println("Condition: ");
-        System.out.println("Condition: " + ctx.BlockID());
-        System.out.println("Attribute: " + ctx.attribute());
-        System.out.println("value: " + ctx.value());
+        currentCondition.setBlock(tkc.getBlock(ctx.BlockID().toString()));
     }
 
-    /*
     @Override
     public void exitCondition(TkcParser.ConditionContext ctx) {
+        currentIf.addCondition(currentCondition);
+        currentCondition = null;
     }
 
     @Override
-    public void enterStatementlist(TkcParser.StatementlistContext ctx) {
+    public void exitComparison(TkcParser.ComparisonContext ctx) {
+        if (ctx.getText().equals("==")) {
+            currentCondition.setComparison(CompType.EQ);
+        } else {
+            currentCondition.setComparison(CompType.EQ);
+        }
     }
-
-    @Override
-    public void exitStatementlist(TkcParser.StatementlistContext ctx) {
-    }
-
+    
     @Override
     public void enterStatement(TkcParser.StatementContext ctx) {
+        currentAction = new Action();
+        currentAction.setActionType(ActionType.ASSIGN); // for now just assignments
+        currentAction.setBlock(tkc.getBlock(ctx.BlockID().toString()));
     }
 
     @Override
     public void exitStatement(TkcParser.StatementContext ctx) {
-    }
-
-    @Override
-    public void enterAttribute(TkcParser.AttributeContext ctx) {
+        if (currentAction != null) {
+            currentIf.addAction(currentAction);
+        }
+        currentAction = null;
     }
 
     @Override
     public void exitAttribute(TkcParser.AttributeContext ctx) {
-    }
-
-    @Override
-    public void enterAssignedattribute(TkcParser.AssignedattributeContext ctx) {
+        String value = ctx.getText().toLowerCase();
+        if (currentCondition != null ) { // condition
+            if (value.equals("occupancy")) {
+                currentCondition.setAttrib(AttributeType.OCCUPANCY);
+            } else if (value.equals("switchstate")) {
+                currentCondition.setAttrib(AttributeType.SWITCHSTATE);
+            }
+        } 
     }
 
     @Override
     public void exitAssignedattribute(TkcParser.AssignedattributeContext ctx) {
-    }
-
-    @Override
-    public void enterValue(TkcParser.ValueContext ctx) {
+        String value = ctx.getText().toLowerCase();
+        if (currentAction != null ) {
+            if (value.equals("occupancy")) {
+                currentAction.setAttrib(AttributeType.OCCUPANCY);
+            } else if (value.equals("speed")) {
+                currentAction.setAttrib(AttributeType.SPEED);
+            } else if (value.equals("authority")) {
+                currentAction.setAttrib(AttributeType.AUTHORITY);
+            } else if (value.equals("crossingstate")) {
+                currentAction.setAttrib(AttributeType.CROSSINGSTATE);
+            } else if (value.equals("switchstate")) {
+                currentAction.setAttrib(AttributeType.SWITCHSTATE);
+            }
+        }
     }
 
     @Override
     public void exitValue(TkcParser.ValueContext ctx) {
+        String value = ctx.getText().toLowerCase();
+        if (currentCondition != null) { // condition value
+            if (value.equals("occupied")) {
+                currentCondition.setValue(true);
+            } else if (value.equals("unoccupied")) {
+                currentCondition.setValue(false);
+            } else if (value.equals("main")) {
+                currentCondition.setValue(true);
+            } else if (value.equals("fork")) {
+                currentCondition.setValue(false);
+            }
+        } else if (currentAction != null ) {
+            currentAction.setValue(value);
+        }
     }
-
-
-    @Override
-    public void enterEveryRule(ParserRuleContext ctx) {
-    }
-
-    @Override
-    public void exitEveryRule(ParserRuleContext ctx) {
-    }
-
-    @Override
-    public void visitTerminal(TerminalNode node) {
-    }
-
     @Override
     public void visitErrorNode(ErrorNode node) {
+        parsingError = true;
     }
-
-*/
-
 }
