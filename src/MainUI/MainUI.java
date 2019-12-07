b9e38d357ce844a6aae3bac80d6c7d487157fdd8
jem325 <50846926+jem325@users.noreply.github.com>
2019-12-07 01:32:12 +0000

    ctc added

--- src/MainUI/MainUI.java
+++ src/MainUI/MainUI.java
@@ -130,9 +130,9 @@ public class MainUI extends Application {
 
         new AnimationTimer() { // anonymous animation timer
             public void handle(long currentNanoTime) {
-                double deltaTSec = (currentNanoTime - startNanoTime)/1000000000.0;
+                //double deltaT = (currentNanoTime - startNanoTime)/1000000000.0;
                 if (updateBoolean) {
-					System.out.println(deltaTSec);
+
                     // update all modules in succession
                     ctc.update();
                     tkcm.update();
