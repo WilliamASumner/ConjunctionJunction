35661c15234750384a46bc9525526b8d3c170ea7
Will Sumner <williamasumner@gmail.com>
2019-12-09 12:10:17 -0500

    fixed for unix

--- check.sh
+++ check.sh
@@ -2,8 +2,11 @@
 var=`java --version | grep "openjdk 13"`
 
 if [ -z "$var" ]; then
-    echo "ERROR: openjdk 13 required"
-    exit 1
+    var=`java --version | grep "java 9.0.1"`
+    if [ -z "$var" ]; then 
+        echo "ERROR: openjdk 13 required"
+        exit 1
+    fi
 else
     echo "openjdk 13 found"
 fi
