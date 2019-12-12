4403daeb8816c701986954daa5b9d9dd5050a64a
Will Sumner <williamasumner@gmail.com>
2019-12-12 11:50:08 -0500

    added switching light color

--- src/TkC/TrackControllerGUI.java
+++ src/TkC/TrackControllerGUI.java
@@ -505,25 +505,33 @@ public class TrackControllerGUI extends Application {
         // UI greying out
         //System.out.println("Current block type is" + currentBlock.getType());
         if (currentBlock.getType() == BlockType.SWITCHBLOCK) {
-            if (currentBlock.getSwitchState() == SwitchState.MAIN) {
-                switchImageView.setImage(switchImageMain);
-                light1.setFill(Color.GREEN);
-                light2.setFill(Color.GREEN);
-                light3.setFill(Color.RED);
+                if (currentBlock.getSwitchState() == SwitchState.MAIN) {
+                    switchImageView.setImage(switchImageMain);
+                    light1.setFill(Color.GREEN);
+                    light2.setFill(Color.GREEN);
+                    light3.setFill(Color.RED);
+                }
+                else {
+                    switchImageView.setImage(switchImageFork);
+                    light1.setFill(Color.GREEN);
+                    light2.setFill(Color.RED);
+                    light3.setFill(Color.GREEN);
+                }
+            if (currentBlock.getSwitching()) { // override to showing switching
+                    light1.setFill(Color.YELLOW);
+                    light2.setFill(Color.YELLOW);
+                    light3.setFill(Color.YELLOW);
             }
-            else {
-                switchImageView.setImage(switchImageFork);
-                light1.setFill(Color.GREEN);
-                light2.setFill(Color.RED);
-                light3.setFill(Color.GREEN);
-            }
-            crossImageView.setImage(crossImageGrey);
+
+            crossImageView.setImage(crossImageGrey); // grey out crossing
         } else if (currentBlock.getType() == BlockType.CROSSBLOCK) {
-            if (currentBlock.getCrossingState() == CrossingState.UP)
+            if (currentBlock.getCrossingState() == CrossingState.UP) {
                 crossImageView.setImage(crossImageUp);
-            else
+            } else {
                 crossImageView.setImage(crossImageDown);
+            }
             switchImageView.setImage(switchImageGrey);
+
             light1.setFill(Color.GREY);
             light2.setFill(Color.GREY);
             light3.setFill(Color.GREY);
